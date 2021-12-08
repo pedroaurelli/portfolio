a15be03b8774159ce02a668c444e5b2b04a6895e
Pedro Aureliano <pedroaureliano.contato@gmail.com>
2021-12-08 14:10:48 -0300

    section sobre pronta e menu mobile pronto

--- script/script.js
+++ script/script.js
@@ -1,10 +1,31 @@
-const btnMenu = document.querySelector('#botaoMenu')
+const btnMenu = document.querySelector('#botaoMenu');
+const header = document.querySelector('.menuHeader');
 
 
 function toggleMenu(){
-    const header = document.querySelector('.menuHeader');
     header.classList.toggle('active'); 
 }
 
-
 btnMenu.addEventListener('click', toggleMenu);
+
+
+
+const linksInternos = document.querySelectorAll('.listaMenu a')
+
+linksInternos.forEach(item =>{
+    item.addEventListener('click', removeActive)
+})
+
+function removeActive(){
+    header.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
+
