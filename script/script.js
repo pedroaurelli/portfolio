13813f0331c2b145af378ae08d2e3c2a972f6eb0
Pedro Aureliano <pedroaureliano.contato@gmail.com>
2021-12-16 13:10:23 -0300

    alterações e animação scroll

--- script/script.js
+++ script/script.js
@@ -1,24 +1,57 @@
-const btnMenu = document.querySelector('#botaoMenu');
-const header = document.querySelector('.menuHeader');
+
+menuMobile();
+
+function menuMobile(){
+
+        const btnMenu = document.querySelector('#botaoMenu');
+        const header = document.querySelector('.menuHeader');
 
 
-function toggleMenu(){
-    header.classList.toggle('active'); 
+        function toggleMenu(){
+            header.classList.toggle('active'); 
+        }
+
+        btnMenu.addEventListener('click', toggleMenu);
+
+
+
+        const linksInternos = document.querySelectorAll('.listaMenu a')
+
+        linksInternos.forEach(item =>{
+            item.addEventListener('click', removeActive)
+        })
+
+        function removeActive(){
+            header.classList.remove('active');
+        }
+
 }
 
-btnMenu.addEventListener('click', toggleMenu);
 
 
 
-const linksInternos = document.querySelectorAll('.listaMenu a')
 
-linksInternos.forEach(item =>{
-    item.addEventListener('click', removeActive)
-})
 
-function removeActive(){
-    header.classList.remove('active');
+initAnimacaoScroll();
+
+function initAnimacaoScroll(){
+    const conteudo = document.querySelectorAll('.js-section')
+    const metadeTela = window.innerHeight * 0.7; 
+
+    function animarScroll(){
+        conteudo.forEach((item) =>{
+            const sectionTop = item.getBoundingClientRect().top;
+            const metadeVh = (metadeTela - sectionTop) 
+            if (metadeVh > 0){
+                item.classList.add('animarScroll')
+            }
+            
+        })
+    }
+
+    window.addEventListener('scroll', animarScroll)
 }
+
 
 
 
